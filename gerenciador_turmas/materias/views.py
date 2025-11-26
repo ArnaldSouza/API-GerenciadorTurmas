a6b9ce3415cd1be04b375095954e9010a6c90680
ArnaldSouza <arnaldsouza472@outlook.com.br>
2025-11-26 20:16:26 -0300

    adicionado serializer, view e rotas para todos os models

--- gerenciador_turmas/materias/views.py
+++ gerenciador_turmas/materias/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Materia
+from .serializers import MateriaSerializer
 
-# Create your views here.
+class ProfessorViewSet(viewsets.ModelViewSet):
+    queryset = Materia.objects.all()
+    serializer_class = MateriaSerializer
+
--- gerenciador_turmas/professores/views.py
+++ gerenciador_turmas/professores/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Professor
+from .serializers import ProfessorSerializer
 
-# Create your views here.
+class ProfessorViewSet(viewsets.ModelViewSet):
+    queryset = Professor.objects.all()
+    serializer_class = ProfessorSerializer
